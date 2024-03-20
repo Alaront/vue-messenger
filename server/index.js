c55c1989f9473868600f29945cf79dc4f2951fea
Alexander Prikhodko <alaront.work@gmail.com>
2024-03-20 09:42:39 +0300

    BACK: static

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 import express from 'express'
+import path from "path";
+import { dirname } from 'path';
+import {fileURLToPath} from "url";
 
 const app = express()
 
-const PORT =  3000
+const PORT =  3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
+
+app.use(express.static(path.resolve(__dirname, 'static')))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
